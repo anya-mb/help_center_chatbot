0d89f35f3575f4ae21eddd917bc78490a044ae4d
anya-mb <anyamb@yandex.ru>
2024-08-19 12:48:36 -0700

    Added Chroma vector store

---
+++ src/generative_ai_utils.py
@@ -0,0 +1,76 @@
+from dotenv import load_dotenv, find_dotenv
+import os
+import chromadb
+from llama_index.core import VectorStoreIndex, SimpleDirectoryReader
+from llama_index.vector_stores.chroma import ChromaVectorStore
+from llama_index.core import StorageContext
+import openai
+
+import warnings
+warnings.filterwarnings("ignore")
+
+# Load environment variables from .env file
+_ = load_dotenv(find_dotenv())
+
+# Set OpenAI API key
+openai.api_key = os.environ["OPENAI_API_KEY"]
+
+CHROMA_DB_PERSISTENT_PATH = '../data/chroma_db'
+CHROMA_DB_COLLECTION_NAME = "bills_faqs"
+
+PARSED_PATH = '../data/Bill_FAQs/parsed'
+
+
+def create_vector_store(
+        document_path: str = PARSED_PATH,
+        chroma_path: str = CHROMA_DB_PERSISTENT_PATH,
+        collection_name: str = CHROMA_DB_COLLECTION_NAME
+):
+    # load some documents
+    documents = SimpleDirectoryReader(document_path).load_data()
+
+    # initialize client, setting path to save data
+    db = chromadb.PersistentClient(path=chroma_path)
+
+    # create collection
+    chroma_collection = db.get_or_create_collection(collection_name)
+
+    # assign chroma as the vector_store to the context
+    vector_store = ChromaVectorStore(chroma_collection=chroma_collection)
+    storage_context = StorageContext.from_defaults(vector_store=vector_store)
+
+    # create your index
+    index = VectorStoreIndex.from_documents(
+        documents, storage_context=storage_context
+    )
+
+
+def load_vector_store(
+        chroma_path: str = CHROMA_DB_PERSISTENT_PATH,
+        collection_name: str = CHROMA_DB_COLLECTION_NAME
+) -> object:
+    # initialize client
+    db = chromadb.PersistentClient(path=chroma_path)
+
+    # get collection
+    chroma_collection = db.get_or_create_collection(collection_name)
+
+    # assign chroma as the vector_store to the context
+    vector_store = ChromaVectorStore(chroma_collection=chroma_collection)
+    storage_context = StorageContext.from_defaults(vector_store=vector_store)
+
+    # load your index from stored vectors
+    index = VectorStoreIndex.from_vector_store(
+        vector_store, storage_context=storage_context
+    )
+    return index
+
+
+def create_query_engine(index: object) -> object:
+    # create a query engine
+    query_engine = index.as_query_engine()
+
+    return query_engine
+
+
+
--- src/help_center_chatbot.py
+++ src/help_center_chatbot.py
@@ -1,10 +1,16 @@
 import logging
+from src.generative_ai_utils import load_vector_store, create_query_engine
 
 
 # Configure logging
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
 
+index = load_vector_store()
+query_engine = create_query_engine(index)
+
 
 def generate_response(question: str) -> str:
-    return "Here is the answer!"
+    response = query_engine.query(question)
+
+    return response.response
